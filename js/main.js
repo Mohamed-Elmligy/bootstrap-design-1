c821807bf6f827713c03cba75d1ae059ef0b8519
Mohamed <mohamedadel485@outlook.sa>
2020-10-02 23:06:43 +0200

    add top button

--- js/main.js
+++ js/main.js
@@ -25,5 +25,25 @@ $(function () {
             $('.shuffle-imges .col-md-12 img').css('opacity', '.08'); 
             $($(this).data('class')).css('opacity', 1); 
         }
-    });
+    });  
 });
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
